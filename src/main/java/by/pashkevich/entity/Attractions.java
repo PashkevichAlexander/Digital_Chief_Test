741ad5b46e315a0882bf4fad08f4164abfafdc0e
PashkevichAlexander <pashkevich.a.a@nail.ru>
2023-06-26 10:22:22 +0300

    refactor read me

--- src/main/java/by/pashkevich/entity/Attractions.java
+++ src/main/java/by/pashkevich/entity/Attractions.java
@@ -4,8 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import org.hibernate.annotations.OnDelete;
-import org.hibernate.annotations.OnDeleteAction;
+
 
 
 import javax.persistence.*;
@@ -34,6 +33,5 @@ public class Attractions implements Serializable {
 
     @ManyToOne
     @JoinColumn
-//    @OnDelete(action = OnDeleteAction.CASCADE)
     private City city;
 }
--- src/main/java/by/pashkevich/entity/Restaurants.java
+++ src/main/java/by/pashkevich/entity/Restaurants.java
@@ -4,8 +4,6 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import org.hibernate.annotations.OnDelete;
-import org.hibernate.annotations.OnDeleteAction;
 
 import javax.persistence.*;
 import java.io.Serializable;
@@ -29,6 +27,5 @@ public class Restaurants implements Serializable {
 
     @ManyToOne
     @JoinColumn
-//    @OnDelete(action = OnDeleteAction.CASCADE)
     private City city;
 }
